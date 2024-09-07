1f0220fae3fc9cb07f6c370e22dfbf01f592f8e9
mottzz87 <57702920+mottzz87@users.noreply.github.com>
2024-09-07 18:30:51 +0800

    Update kktv.js

--- rewrite/kktv.js
+++ rewrite/kktv.js
@@ -1,3 +1,9 @@
+[rewrite_local]
+^https:\/\/api\.kktv\.me\/v3\/ipcheck url script-response-body https://raw.githubusercontent.com/mottzz87/crules/main/rewrite/kktv.js
+
+[mitm]
+hostname = api.kktv.me
+
 let originBody = JSON.parse($response.body)
 
 console.log('originBody--------', originBody)
