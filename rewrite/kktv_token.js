4ef7a4639982b690508e60c7fe2ed002ae9924f9
mottzz87 <57702920+mottzz87@users.noreply.github.com>
2024-09-07 22:34:00 +0800

    Update kktv_token.js

--- rewrite/kktv_token.js
+++ rewrite/kktv_token.js
@@ -11,17 +11,7 @@
         subtype: null,
         message: null,
       },
-      data: {
-        user_id: 'c0a8eada-59e1-4f82-b145-73f5b0a061e1',
-        device_id: '737eabc7-0930-4530-9824-56a0a42049d6',
-        medium: 'SVOD',
-        title_id: '01000718',
-        episode_id: '01000718010001',
-        key_id: '1f386559d3244cb815ccf0aabd6fef0b',
-        playback_token: '7ff6c5ae89fc4881803a9b9c28a8675978db9f6531d74828a175cca89c276eea',
-        created_at: 1725714319,
-        expires_at: 1725800719,
-      },
+      data: originalBody.data
     }
 
     console.log('修改后的响应：', JSON.stringify(modifiedResponse))
