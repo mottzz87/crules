e88d4fa265973dafebab56d8d9c24c9844ce5c8c
vane <hualuna87@126.com>
2024-09-07 20:20:04 +0800

    feat:t

--- rewrite/kktv.js
+++ rewrite/kktv.js
@@ -1,12 +1,27 @@
-let originBody = JSON.parse($response.body)
+;(() => {
+  try {
+    if (typeof $response === 'undefined') {
+      console.log('错误：$response 未定义')
+      $done({})
+      return
+    }
 
-console.log('originBody--------', originBody)
+    if (!$response.body) {
+      console.log('错误：$response.body 为空')
+      $done({})
+      return
+    }
 
-let response = {
-  data: {
-    is_allowed: true,
-  },
-}
-console.log('modifiedBody---------', response)
+    let response = {
+      data: {
+        is_allowed: true,
+      },
+    }
 
-$done({ body: JSON.stringify(response) })
+    console.log('修改后的响应：', JSON.stringify(response))
+    $done({ body: JSON.stringify(response) })
+  } catch (error) {
+    console.log('脚本执行出错：', error.message)
+    $done({})
+  }
+})()
