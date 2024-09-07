56d0f2000733912fa8baaa26c32cb4dcd6e5bfcd
vane <hualuna87@126.com>
2024-09-07 21:39:05 +0800

    feat:tt

--- rewrite/kktv_token.js
+++ rewrite/kktv_token.js
@@ -2,27 +2,27 @@
   try {
     let originalBody = JSON.parse($response.body)
     console.log('111：', 111)
-    console.log('222：', JSON.stringify({test: 222}))
+    console.log('222：', JSON.stringify({ test: 222 }))
     console.log('原始响应体：', JSON.stringify(originalBody))
 
     let modifiedResponse = {
-      "status": {
-          "type": "OK",
-          "subtype": null,
-          "message": null
+      status: {
+        type: 'OK',
+        subtype: null,
+        message: null,
       },
-      "data": {
-          "user_id": "c0a8eada-59e1-4f82-b145-73f5b0a061e1",
-          "device_id": "",
-          "medium": "SVOD",
-          "title_id": "01000718",
-          "episode_id": "01000718010001",
-          "key_id": "1f386559d3244cb815ccf0aabd6fef0b",
-          "playback_token": "7ff6c5ae89fc4881803a9b9c28a8675978db9f6531d74828a175cca89c276eea",
-          "created_at": 1725714319,
-          "expires_at": 1725800719
-      }
-  }
+      data: {
+        user_id: 'c0a8eada-59e1-4f82-b145-73f5b0a061e1',
+        device_id: '737eabc7-0930-4530-9824-56a0a42049d6',
+        medium: 'SVOD',
+        title_id: '01000718',
+        episode_id: '01000718010001',
+        key_id: '1f386559d3244cb815ccf0aabd6fef0b',
+        playback_token: '7ff6c5ae89fc4881803a9b9c28a8675978db9f6531d74828a175cca89c276eea',
+        created_at: 1725714319,
+        expires_at: 1725800719,
+      },
+    }
 
     console.log('修改后的响应：', JSON.stringify(modifiedResponse))
     $done({ body: JSON.stringify(modifiedResponse) })
