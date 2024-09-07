eb628fdf05d7cd88f503fb62cf475d7623b1f068
vane <hualuna87@126.com>
2024-09-07 20:34:02 +0800

    feat:t

--- rewrite/kktv.js
+++ rewrite/kktv.js
@@ -1,38 +1,30 @@
 ;(() => {
   console.log('脚本开始执行')
 
-  if (typeof $request !== 'undefined') {
-    console.log('这是一个请求脚本')
+  if (typeof $request !== 'undefined' && $request) {
+    console.log('这是一个请求')
+    // 如果需要修改请求，可以在这里进行
     $done({})
-    return
-  }
+  } else if (typeof $response !== 'undefined' && $response) {
+    console.log('这是一个响应')
+    try {
+      let originalBody = JSON.parse($response.body)
+      console.log('原始响应体：', JSON.stringify(originalBody))
 
-  if (typeof $response === 'undefined') {
-    console.log('错误：$response 未定义')
-    $done({})
-    return
-  }
+      let modifiedResponse = {
+        data: {
+          is_allowed: true,
+        },
+      }
 
-  if (!$response.body) {
-    console.log('错误：$response.body 为空')
-    $done({})
-    return
-  }
-
-  try {
-    let originalBody = JSON.parse($response.body)
-    console.log('原始响应体：', JSON.stringify(originalBody))
-
-    let modifiedResponse = {
-      data: {
-        is_allowed: true,
-      },
+      console.log('修改后的响应：', JSON.stringify(modifiedResponse))
+      $done({ body: JSON.stringify(modifiedResponse) })
+    } catch (error) {
+      console.log('脚本执行出错：', error.message)
+      $done({})
     }
-
-    console.log('修改后的响应：', JSON.stringify(modifiedResponse))
-    $done({ body: JSON.stringify(modifiedResponse) })
-  } catch (error) {
-    console.log('脚本执行出错：', error.message)
+  } else {
+    console.log('错误：既不是请求也不是响应')
     $done({})
   }
 })()
