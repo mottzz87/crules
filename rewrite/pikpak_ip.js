47bf4955e1b8f931de91c1eaa56c1146d2002cba
vane <hualuna87@126.com>
2024-11-02 18:01:42 +0800

    feat pikpak update

--- rewrite/pikpak_ip.js
+++ rewrite/pikpak_ip.js
@@ -1,33 +1,24 @@
 ;(() => {
   console.log('PIKPAK 响应脚本执行')
   const url = $request.url
-  const hostname = new URL(url).hostname
-
   try {
     let originalBody = JSON.parse($response.body)
     console.log('原始响应体：', JSON.stringify(originalBody))
 
-    let modifiedResponse
-
-    if (hostname === 'api-drive.mypikpak.com') {
-      if (url.indexOf('area_country_code') !== -1) {
-        modifiedResponse = {
-          data: {
-            countryCode: 'KR',
-            ip: '158.247.245.228',
-          },
-          redirect_uri: '',
-          result: 'ACCEPTED',
-          message: '',
-        }
-      }
-    } else if (hostname === 'access.mypikpak.net') {
-      if (url.indexOf('area_accessible') !== -1) {
-        modifiedResponse = {
-          accessible: true,
-          countryCode: 'KR',
-          ip: '158.247.245.228',
-        }
+    let modifiedResponse = {
+      data: {
+        countryCode: 'KR',
+        ip: '158.247.245.228',
+      },
+      redirect_uri: '',
+      result: 'ACCEPTED',
+      message: '',
+    }
+    if (url.includes('area_accessible')) {
+      modifiedResponse = {
+        accessible: true,
+        countryCode: 'KR',
+        ip: '158.247.245.228',
       }
     }
 
