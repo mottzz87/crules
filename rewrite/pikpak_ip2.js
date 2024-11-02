bad93f8027341706e04d4a77caca712c43513042
vane <hualuna87@126.com>
2024-11-02 18:39:19 +0800

    feat pikpak update

--- rewrite/pikpak_ip2.js
+++ rewrite/pikpak_ip2.js
@@ -6,14 +6,11 @@
     console.log('原始响应体：', JSON.stringify(originalBody))
 
     let modifiedResponse = {
-      data: {
-        countryCode: 'KR',
-        ip: '158.247.245.228',
-      },
-      redirect_uri: '',
-      result: 'ACCEPTED',
-      message: '',
+      accessible: true,
+      countryCode: 'KR',
+      ip: '158.247.245.228',
     }
+
     console.log('修改后的响应：', JSON.stringify(modifiedResponse))
     $done({ body: JSON.stringify(modifiedResponse) })
   } catch (error) {
